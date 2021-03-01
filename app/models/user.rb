17c6be0c0f83f658b24a668272d58865af116fcc
cisaias <cisaias@uninorte.edu.co>
2021-03-01 13:27:21 -0500

    validaciones de correo y contraseña, que no sean nulas

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :email, uniqueness: true
+  validates :email, presence:true
+  validates :password, presence:true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
         
