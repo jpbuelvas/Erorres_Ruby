8d482da4cdbd9e08fd74b57bafd7a6f3d53f8099
cisaias <cisaias@uninorte.edu.co>
2021-03-02 00:50:32 -0500

    heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+ 
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
@@ -77,3 +79,4 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
+
