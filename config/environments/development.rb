5a82f9171e9e403ae6bef275f17da0081941aa69
cisaias <cisaias@uninorte.edu.co>
2021-03-02 00:05:44 -0500

    Validacion de correo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,8 +40,24 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
+
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+
+config.action_mailer.default :charset => "utf-8"
+
+config.action_mailer.smtp_settings = {
+  :authentication => :plain,
+  :address => "smtp.gmail.com",
+  :port => '587',
+  :domain => "gmail.com",
+  :user_name => 'isaiasdiaz132@gmail.com',
+  :password => '123456isaias!'
+}
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
