a7a61cd52d4d291bb56815d16ac3212a709cb84d
cisaias <cisaias@uninorte.edu.co>
2021-03-02 09:34:10 -0500

    config general heroku 3

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,5 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-if Rails.env.development? || Rails.env.production?
 
 #comentario para la rama emails
