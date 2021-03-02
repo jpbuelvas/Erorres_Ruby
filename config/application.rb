770fdf1e22c11d06c88f456e7ddcfdc14d6697f9
cisaias <cisaias@uninorte.edu.co>
2021-03-02 01:46:11 -0500

    ajuste heroku 2.0

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,5 @@ module Back
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.assets.initialize_on_precompile = false
   end
 end
