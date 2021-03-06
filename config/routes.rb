e15c81df07d9548db7d23da3ffdbd3fe12b6611e
cisaias <cisaias@uninorte.edu.co>
2021-03-05 23:42:08 -0500

    ajuste de email

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  Rails.application.routes.default_url_options[:host] = 'tutweet.herokuapp.com'
+  Rails.application.routes.default_url_options[:host] = 'tweetsisaias.herokuapp.com'
   root'home#index'
   get '/tweets', to: 'tweets#index'
   
